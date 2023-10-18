515cc0de2b6047072fce27fda8859e0991c4a3b1
Ctbenoit60 <ctbenoit60@gmail.com>
2023-10-18 17:04:09 -0500

    Question 1 & 2

---
+++ index.js
@@ -0,0 +1,36 @@
+// Question 1
+// 10
+// -1
+// 1
+// 2
+// 6
+// 9px
+// $45
+// 2
+// 2
+//  -9 5
+// -14
+// 1
+// NaN
+// true
+// false
+// -2
+
+// Question 2
+
+let three = "3"
+let four = "4"
+let thirty = "30"
+//what is the value of the following expressions?
+let addition = three + four
+// 34 
+let multiplication = three * four
+// 12
+let division = three / four
+// 0.75
+let subtraction = three - four
+let lessThan1 = three < four
+let lessThan2 = thirty < four
+
+console.log(subtraction);
+
