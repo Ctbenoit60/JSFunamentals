11bb01ddc64937131bcdea7891f9b6254e965158
Ctbenoit60 <ctbenoit60@gmail.com>
2023-10-18 18:24:29 -0500

    answers to question 1

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 // Question 1
-// 10
-// -1
-// 1
-// 2
-// 6
-// 9px
-// $45
-// 2
-// 2
-//  -9 5
-// -14
-// 1
-// NaN
-// true
-// false
-// -2
+"" + 1 + 0 //10
+"" - 1 + 0 //-1
+true + false //1
+!true // 2
+6 / "3" // 2
+"2" * "3" // 6
+4 + 5 + "px" // 9px
+"$" + 4 + 5 // $45
+"4" - 2 // 2
+"4px" - 2 // 2
+" -9 " + 5 //-9 5 
+" -9 " - 5 // -14
+null + 1 // 1
+undefined + 1 // NaN
+undefined == null // true
+undefined === null // false
+" \t \n" - 2 // -2
 
-// Question 2
-
-let three = "3"
-let four = "4"
-let thirty = "30"
-//what is the value of the following expressions?
-let addition = three + four
-// 34 
-let multiplication = three * four
-// 12
-let division = three / four
-// 0.75
-let subtraction = three - four
-let lessThan1 = three < four
-let lessThan2 = thirty < four
-
-console.log(subtraction);
+10
+-1
+1
+2
+6
+9px
+$45
+2
+2
+ -9 5
+-14
+1
+NaN
+true
+false
+-2
 
