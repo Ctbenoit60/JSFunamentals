d2f74e2e086c342e178c20ace7f96dbc403afc98
Ctbenoit60 <ctbenoit60@gmail.com>
2023-10-19 18:08:36 -0500

    Question4

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
-// Question 1
-// 10
-// -1
-// 1
-// 2
-// 6
-// 9px
-// $45
-// 2
-// 2
-//  -9 5
-// -14
-// 1
-// NaN
-// true
-// false
-// -2
-
-// Question 2
-
-let three = "3"
-let four = "4"
-let thirty = "30"
-//what is the value of the following expressions?
-let addition = three + four
-// 34 
-let multiplication = three * four
-// 12
-let division = three / four
-// 0.75
-let subtraction = three - four
-let lessThan1 = three < four
-let lessThan2 = thirty < four
-
-console.log(subtraction);
-
+let a = 2, b = 3;
+let result = `${a} + ${b} is `;
+if (a + b < 10) {
+result += 'less than 10';
+} else {
+result += 'greater than 10';
+}
