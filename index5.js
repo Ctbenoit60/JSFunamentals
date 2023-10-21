9e5b734f98328eb0b6de294ace735c44d42c5e42
Ctbenoit60 <ctbenoit60@gmail.com>
2023-10-20 19:06:38 -0500

    Question5

---
+++ index5.js
@@ -0,0 +1,14 @@
+// Rewrite the following function using: a) function expression syntax, and b) arrow function
+// syntax. Test each version to make sure they work the same.
+
+//a
+const getGreeting = function(name) {
+return 'Hello ' + name + '!';
+}
+
+getGreeting('Christina');
+console.log(getGreeting('Christina'));
+
+//b
+
+
