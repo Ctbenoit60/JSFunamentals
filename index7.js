436b8cd29cc0da32de93d7707e57ff9e2c653fb9
Ctbenoit60 <ctbenoit60@gmail.com>
2023-10-21 16:32:58 -0500

    For question 7 I added returns and a new method so that the final line of could would print the correct consolelog

--- index7.js
+++ index7.js
@@ -3,24 +3,49 @@
 // a) Modify each of the methods so that they can be ‘chained’ together and the last line of
 // the example code works
 // b) Add a new method to print the full time final score
+//  1) new method 2) print the final score
 // c) Add a new object property to keep track of the number of fouls and a method to
 // increment it, similar but separate to the score. Include the foul count in the half time and
 // full time console messages
+// 1) add new key value for fouls 2) add method ot increment it 3) add the fouls to the half time and fill time message
 // d) Test your object by chaining all the method calls together in different combinations.
 const basketballGame = {
-score: 0,
-freeThrow() {
-this.score++;
-},
-basket() {
-this.score += 2;
-},
-threePointer() {
-this.score += 3;
-},
-halfTime() {
-console.log('Halftime score is '+this.score);
-}
-}
+  score: 0,
+  fouls:0,
+  freeThrow() {
+    this.score++;
+    return this;
+  },
+  numFouls() {
+    this.fouls++;
+    return this;
+  },
+  basket() {
+    this.score += 2;
+    // console.log(this.score);
+    return this;
+  },
+  threePointer() {
+    this.score += 3;
+    return this;
+  },
+  halfTime() {
+    console.log("Halftime score is " + this.score + " , number of fouls " + this.fouls);
+    return this;
+  },
+  finalScore() {
+    console.log("Final score is " + this.score + " , number of fouls " + this.fouls);
+  },
+};
 //modify each of the above object methods to enable function chaining as below:
-basketballGame.basket().freeThrow().freeThrow().basket().threePointer().halfTime();
+basketballGame
+  .basket()
+  .freeThrow()
+  .freeThrow()
+  .basket()
+  .threePointer()
+  .halfTime()
+  .numFouls()
+  .numFouls()
+  .basket()
+  .finalScore();
