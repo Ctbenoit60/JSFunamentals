6f6e2ec0a1608cefc19460b8ffcd686735437f71
Ctbenoit60 <ctbenoit60@gmail.com>
2023-10-22 14:46:11 -0500

    added a new function and a forin loop

--- index8.js
+++ index8.js
@@ -5,19 +5,41 @@
 // b) Create a new object for a different city with different properties and call your function
 // again with the new object.
 const sydney = {
-name: 'Sydney',
-population: 5_121_000,
-state: 'NSW',
-founded: '26 January 1788',
-timezone: 'Australia/Sydney'
+  name: "Sydney",
+  population: 5_121_000,
+  state: "NSW",
+  founded: "26 January 1788",
+  timezone: "Australia/Sydney",
+};
+
+const lafayette = {
+  name: "Lafayette",
+  originalName: "Vermilionville",
+  population: 121_771,
+  state: "Louisiana",
+  founded: "1821 by Jean Mouton",
+  timezone: "Central",
+};
+
+// for (let key in sydney) {
+//     console.log( key + " = " + sydney[key]);
+// }
+
+function whatCity(city) {
+  for (let key in city) {
+    console.log(key + " = " + city[key]);
+  }
 }
 
-function whatCity(sydney) {
-    for (const key in sydney) {
-        if (Object.hasOwnProperty.call(sydney, key)) {
-            const element = object[key];
-            
-        }
-    }
-    
-}
+whatCity(lafayette);
+whatCity(sydney);
+
+// const phone = {
+// model: 'iPhone 11',
+// color: 'black',
+// storage: 64
+// }
+// for (let key in phone) { // iterates over each property in the phone object
+// console.log('key: ' + key); // prints each object property name (key) in turn
+// console.log('value: ' + phone[key]); // prints each object value in turn
+// }
