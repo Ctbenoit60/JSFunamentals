2c751741066332089f2a0c4457afa27b88f24d36
Ctbenoit60 <ctbenoit60@gmail.com>
2023-10-22 20:29:55 -0500

    added the answer breakdown

--- index9.js
+++ index9.js
@@ -11,11 +11,14 @@
 let teamSports = ['Hockey', 'Cricket', 'Volleyball'];
 let moreSports = teamSports.push('football')
 teamSports.unshift('soccer') // part a: a new variable that adds to the teamSports array using the push method and unshift method
-console.log(teamSports);
+console.log(teamSports); //part d: the push and shift method adds on 2 addional sports 
 
 let dog1 = 'Bingo';
 let dog2 = dog1
- dog1 = 'Dingo'
-console.log(dog1);//part b: created a second variable and equated it to dog1 which allowed us to give dog1 a new variable
+ dog1 = 'Dingo'//part b: created a second variable and equated it to dog1 which allowed us to give dog1 a new variable
+console.log(dog1);//part d: Bc we changed Bingo to Dingo by creating a reference to the old name
 
 let cat1 = { name: 'Fluffy', breed: 'Siberian' };
+let cat2 = {...cat1};// part c: created a shallow clone of cat 1 and then changed the name property for cat1
+cat1.name = 'Floffy';
+console.log(cat1); // part d: After creating a shallow clone, we wre able to change the name property
